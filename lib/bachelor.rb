0a5d6257ef3c448e6707f3ef31d98305584cee8b
Rob Parker <into.the.parker@gmail.com>
2018-09-12 14:43:05 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,17 +1,45 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |keys|
+    keys.each do |i,v|
+      if v == 'Winner'
+          return keys["name"].split(' ')[0].to_s
+        end
+      end   
+  end  
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+      data.values.each do |values|
+        values.each do |i|
+          if i["occupation"] == occupation
+            return i["name"]
+          end  
+        end
+      end  
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  homeCounter = 0
+    data.values.each do |i|
+      i.each do |t|
+        if t["hometown"] == hometown
+        homeCounter += 1
+        end
+      end
+    end 
+homeCounter
 end
 
 def get_occupation(data, hometown)
-  # code here
+    data.each do |i,k|
+      k.each do |t|
+        t.each do |p, j|
+          if j == hometown
+            return t["name"]
+          end  
+        end
+      end
+    end
 end
 
 def get_average_age_for_season(data, season)
